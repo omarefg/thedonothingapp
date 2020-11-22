aeccf79c4648737e0ee5e3144a6e5c3a5aa8db2e
Omar Flores Grimontt <dev@omarefg.com>
2020-11-22 10:48:03 -0500

    added corners example from basic real world

--- app/modules/animations/basic-real-world/corners/Corners.js
+++ app/modules/animations/basic-real-world/corners/Corners.js
@@ -3,12 +3,73 @@
  */
 
 import React from 'react';
-import { Text, View } from 'react-native';
+import {
+  Animated,
+  Dimensions,
+  Pressable,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  box: {
+    width: 150,
+    height: 150,
+    backgroundColor: 'tomato',
+    position: 'absolute',
+    top: -75,
+    left: 0,
+  },
+});
 
 export function BasicRealWorldCorners(): React$Node {
+  const animation = new Animated.ValueXY();
+  const dimensions = { width: 0, height: 0 };
+  const startAnimation = () => {
+    const { width, height } = Dimensions.get('window');
+
+    Animated.sequence([
+      Animated.spring(animation.y, {
+        toValue: height - dimensions.height,
+        useNativeDriver: false,
+      }),
+      Animated.spring(animation.x, {
+        toValue: width - dimensions.width,
+        useNativeDriver: false,
+      }),
+      Animated.spring(animation.y, {
+        toValue: 0,
+        useNativeDriver: false,
+      }),
+      Animated.spring(animation.x, {
+        toValue: 0,
+        useNativeDriver: false,
+      }),
+    ]).start();
+  };
+
+  const saveDimensions = (event) => {
+    dimensions.width = event.nativeEvent.layout.width;
+    dimensions.height = event.nativeEvent.layout.height;
+  };
+
+  const animatedStyles = {
+    transform: animation.getTranslateTransform(),
+  };
+
   return (
-    <View style={{ flex: 1, backgroundColor: 'green' }}>
-      <Text>BasicRealWorldCorners</Text>
+    <View style={styles.container}>
+      <Pressable
+        onPress={startAnimation}
+      >
+        <Animated.View
+          style={[styles.box, animatedStyles]}
+          onLayout={saveDimensions}
+        />
+      </Pressable>
     </View>
   );
 }
